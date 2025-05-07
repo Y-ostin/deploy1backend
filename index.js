b69d0d62eb9e875954fa36c38e7e185f78dff020
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2025-05-07 14:00:47 -0500

    feat : add front-end

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
 require('dotenv').config();
 const http = require('http');
 
 function requestController(req, res) {
-  console.log('Bienvenidos al curso')   
+    if (req.url === '/' && req.method === 'GET') {
+        const filePath = path.join(__dirname, 'public', 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Página no encontrada');
+    }
 }
 
 const server = http.createServer(requestController);
